e8f3a195a0eda2d1bf06de9b6250149b0fc604dd
zavalachumpitazoscar <zavalachumpitazoscar@gmail.com>
2026-06-23 11:04:27 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCX3M6ttukx2nsKE4g3WkpqOTS9ECR_gvM",
+
+  authDomain: "control-asistencia-82fe2.firebaseapp.com",
+
+  projectId: "control-asistencia-82fe2",
+
+  storageBucket: "control-asistencia-82fe2.firebasestorage.app",
+
+  messagingSenderId: "861657394944",
+
+  appId: "1:861657394944:web:9815dee357ee203fb00de5"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
