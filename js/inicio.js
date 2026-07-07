777e842e5eae517b6db874b01aba31f43d425ad1
zavalachumpitazoscar <zavalachumpitazoscar@gmail.com>
2026-07-07 16:17:13 -0500

    Update inicio.js

--- js/inicio.js
+++ js/inicio.js
@@ -1,3 +1,195 @@
+import { auth } from "./firebase.js";
+
+import {
+onAuthStateChanged,
+signOut
+}
+from
+"https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+
+
+
+
+const sidebar =
+document.querySelector(".sidebar");
+
+
+const botonMenu =
+document.querySelector(".btn-menu");
+
+
+
+const botones =
+document.querySelectorAll(".item");
+
+
+const contenedor =
+document.getElementById(
+"contenedorVista"
+);
+
+
+
+const titulo =
+document.querySelector(
+".topbar h1"
+);
+
+
+
+
+// ============================
+// PROTEGER PAGINA
+// ============================
+
+
+onAuthStateChanged(auth,(usuario)=>{
+
+
+    if(!usuario){
+
+
+        window.location.href =
+        "login.html";
+
+
+        return;
+
+
+    }
+
+
+    cargarVista(
+        "dashboard"
+    );
+
+
+});
+
+
+
+
+
+// ============================
+// MENU LATERAL
+// ============================
+
+
+botonMenu.addEventListener(
+"click",
+()=>{
+
+
+sidebar.classList.toggle(
+"mostrar"
+);
+
+
+});
+
+
+
+
+
+
+botones.forEach(btn=>{
+
+
+btn.addEventListener(
+"click",
+()=>{
+
+
+botones.forEach(b=>
+b.classList.remove(
+"activo"
+)
+);
+
+
+
+btn.classList.add(
+"activo"
+);
+
+
+
+titulo.textContent =
+btn.innerText;
+
+
+
+cargarVista(
+btn.dataset.vista
+);
+
+
+
+// cerrar menú en celular
+
+sidebar.classList.remove(
+"mostrar"
+);
+
+
+
+});
+
+
+});
+
+
+
+
+
+async function cargarVista(vista){
+
+
+const respuesta =
+await fetch(
+`vistas/${vista}.html`
+);
+
+
+const html =
+await respuesta.text();
+
+
+
+contenedor.innerHTML =
+html;
+
+
+}
+
+
+
+
+
+// ============================
+// CERRAR SESIÓN
+// ============================
+
+
+document
+.querySelector(".salir")
+.addEventListener(
+"click",
+async()=>{
+
+
+await signOut(auth);
+
+
+window.location.href =
+"login.html";
+
+
+});
+
+
+
+
 const botones =
 document.querySelectorAll(".item");
 
