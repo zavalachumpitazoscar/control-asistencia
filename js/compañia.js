e43d5a67d54a45bf9e7f635d84a6d408d2c5d0a6
zavalachumpitazoscar <zavalachumpitazoscar@gmail.com>
2026-07-08 10:09:27 -0500

    Update compañia.js

--- js/compañia.js
+++ js/compañia.js
@@ -1,5 +1,48 @@
-export function iniciarCompañia(){
+export function iniciarCompania(){
 
-    console.log("Módulo Compañía iniciado.");
+    const botones =
+        document.querySelectorAll(".tab");
+
+    const paneles =
+        document.querySelectorAll(".panel-tab");
+
+
+
+    botones.forEach(boton=>{
+
+        boton.addEventListener("click",()=>{
+
+            botones.forEach(b=>{
+
+                b.classList.remove("activo");
+
+            });
+
+
+            paneles.forEach(panel=>{
+
+                panel.classList.add("oculto");
+
+            });
+
+
+
+            boton.classList.add("activo");
+
+
+
+            const nombre =
+                boton.dataset.tab;
+
+
+
+            document
+            .getElementById(nombre)
+            .classList
+            .remove("oculto");
+
+        });
+
+    });
 
 }
