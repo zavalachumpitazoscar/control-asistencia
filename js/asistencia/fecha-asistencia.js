a3c0d3b81dcf997ab1686290aa7499a2f062dcaa
zavalachumpitazoscar <zavalachumpitazoscar@gmail.com>
2026-07-17 16:02:32 -0500

    Create fecha-asistencia.js

---
+++ js/asistencia/fecha-asistencia.js
@@ -0,0 +1,48 @@
+<div class="asistencia-selector-fecha">
+
+    <button
+        type="button"
+        class="btn-fecha-navegacion"
+        id="btnDiaAnteriorAsistencia"
+        title="Día anterior"
+    >
+
+        <i class="bi bi-chevron-left"></i>
+
+    </button>
+
+
+    <div class="asistencia-fecha-actual">
+
+        <span>
+            Fecha seleccionada
+        </span>
+
+        <strong id="fechaActualAsistencia">
+            Viernes, 17 de julio de 2026
+        </strong>
+
+    </div>
+
+
+    <button
+        type="button"
+        class="btn-fecha-navegacion"
+        id="btnDiaSiguienteAsistencia"
+        title="Día siguiente"
+    >
+
+        <i class="bi bi-chevron-right"></i>
+
+    </button>
+
+
+    <button
+        type="button"
+        class="btn-asistencia-hoy"
+        id="btnHoyAsistencia"
+    >
+        Hoy
+    </button>
+
+</div>
