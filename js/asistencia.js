d1788feb72ba70f0fd4f05e49b87c111419c8243
zavalachumpitazoscar <zavalachumpitazoscar@gmail.com>
2026-07-17 15:53:09 -0500

    Update asistencia.js

--- js/asistencia.js
+++ js/asistencia.js
@@ -1,1 +1,37 @@
+import {
+    iniciarNavegacionAsistencia
+}
+from "./navegacion-asistencia.js";
 
+
+import {
+    iniciarFechaAsistencia
+}
+from "./fecha-asistencia.js";
+
+
+import {
+    iniciarImportacionMarcaciones
+}
+from "./importacion-marcaciones.js";
+
+
+
+/*=====================================================
+INICIAR MÓDULO DE ASISTENCIA
+=====================================================*/
+
+export async function iniciarAsistencia(){
+
+    console.log(
+        "✅ INICIANDO MÓDULO DE ASISTENCIA"
+    );
+
+
+    iniciarNavegacionAsistencia();
+
+    iniciarFechaAsistencia();
+
+    iniciarImportacionMarcaciones();
+
+}
