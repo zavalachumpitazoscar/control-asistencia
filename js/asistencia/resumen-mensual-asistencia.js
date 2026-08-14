161bb48d1d2f11550eca00c8031866b5cc3243b9
zavalachumpitazoscar <zavalachumpitazoscar@gmail.com>
2026-08-14 10:59:02 -0500

    Update resumen-mensual-asistencia.js

--- js/asistencia/resumen-mensual-asistencia.js
+++ js/asistencia/resumen-mensual-asistencia.js
@@ -1,7 +1,7 @@
 import {
   consultarColeccionEmpresa,
   construirRegistrosResumen,
-} from "./resumen-asistencia.js";
+} from "./resumen-asistencia.js?v=20260814-1";
 
 let fechaDesde, fechaHasta, buscarResumen, cuerpoResumen, btnActualizar;
 let paginaResumenMensual = 1;
