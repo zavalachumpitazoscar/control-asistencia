80baa2af917f67e465fc52a43aa0d01bdca98091
zavalachumpitazoscar <zavalachumpitazoscar@gmail.com>
2026-08-17 16:20:08 -0500

    Update inicio.js

--- js/inicio.js
+++ js/inicio.js
@@ -26,8 +26,8 @@ from
 "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
 
 import { iniciarCompañia } from "./compañia.js?v=20260817-1";
-import { iniciarEmpleados } from "./empleados.js?v=20260817-5";
-import { iniciarAsistencia } from "./asistencia.js?v=20260817-4";
+import { iniciarEmpleados } from "./empleados.js?v=20260817-6";
+import { iniciarAsistencia } from "./asistencia.js?v=20260817-5";
 import { iniciarAuditoria, iniciarMonitorAuditoriaGlobal } from "./auditoria.js?v=20260814-2";
 import { iniciarDashboard } from "./dashboard.js?v=20260817-5";
 import { iniciarConfiguracion, aplicarAparienciaGuardada, obtenerAparienciaGuardada } from "./configuracion.js?v=20260814-1";
