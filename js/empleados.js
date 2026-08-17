489c7680b21fb802d693de8095d11e8bb82d7b42
zavalachumpitazoscar <zavalachumpitazoscar@gmail.com>
2026-08-17 16:20:20 -0500

    Update empleados.js

--- js/empleados.js
+++ js/empleados.js
@@ -2,7 +2,7 @@ import { iniciarColaboradores } from "./empleados/colaboradores.js?v=20260817-3"
 import { iniciarHorarios } from "./empleados/horarios.js?v=20260814-4";
 import { iniciarPermisos } from "./empleados/permisos.js";
 import { iniciarFeriados } from "./empleados/feriados.js?v=20260814-1";
-import { iniciarAdministracionMarcacionMovil } from "./empleados/marcacion-movil.js?v=20260817-5";
+import { iniciarAdministracionMarcacionMovil } from "./empleados/marcacion-movil.js?v=20260817-6";
 
 export function iniciarEmpleados(
     tabInicial = "colaboradores"
