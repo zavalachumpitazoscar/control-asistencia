4ee468693f29040689d02080b891f666c2d877f2
zavalachumpitazoscar <zavalachumpitazoscar@gmail.com>
2026-08-17 12:49:36 -0500

    Update inicio.js

--- js/inicio.js
+++ js/inicio.js
@@ -25,11 +25,11 @@ signOut
 from
 "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
 
-import { iniciarCompañia } from "./compañia.js";
+import { iniciarCompañia } from "./compañia.js?v=20260817-1";
 import { iniciarEmpleados } from "./empleados.js?v=20260814-4";
 import { iniciarAsistencia } from "./asistencia.js?v=20260814-3";
 import { iniciarAuditoria, iniciarMonitorAuditoriaGlobal } from "./auditoria.js?v=20260814-2";
-import { iniciarDashboard } from "./dashboard.js?v=20260814-4";
+import { iniciarDashboard } from "./dashboard.js?v=20260817-1";
 import { iniciarConfiguracion, aplicarAparienciaGuardada, obtenerAparienciaGuardada } from "./configuracion.js?v=20260814-1";
 import { iniciarManual, abrirManual } from "./manual.js?v=20260814-3";
 
@@ -430,7 +430,7 @@ async function cargarVista(
 
 
         const ruta =
-            `vistas/${vistaPrincipal}.html?v=20260814-3`;
+            `vistas/${vistaPrincipal}.html?v=20260817-1`;
 
 
         const respuesta =
