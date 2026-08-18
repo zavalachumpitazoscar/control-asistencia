da2212460e3061ac52846ba4b93eeb62a45c6b9d
zavalachumpitazoscar <zavalachumpitazoscar@gmail.com>
2026-08-18 12:36:02 -0500

    Update import version for empleados.js

--- js/inicio.js
+++ js/inicio.js
@@ -26,7 +26,7 @@ from
 "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
 
 import { iniciarCompañia } from "./compañia.js?v=20260817-1";
-import { iniciarEmpleados } from "./empleados.js?v=20260818-1";
+import { iniciarEmpleados } from "./empleados.js?v=20260818-2";
 import { iniciarAsistencia } from "./asistencia.js?v=20260817-7";
 import { iniciarAuditoria, iniciarMonitorAuditoriaGlobal } from "./auditoria.js?v=20260814-2";
 import { iniciarDashboard } from "./dashboard.js?v=20260817-7";
