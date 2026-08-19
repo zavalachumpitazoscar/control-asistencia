5b069ee52bd7c3b92dff6c2eba6989d863495d89
zavalachumpitazoscar <zavalachumpitazoscar@gmail.com>
2026-08-19 11:48:20 -0500

    Update import versions and fetch URL parameters

--- js/inicio.js
+++ js/inicio.js
@@ -26,8 +26,8 @@ from
 "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
 
 import { iniciarCompañia } from "./compañia.js?v=20260817-1";
-import { iniciarEmpleados } from "./empleados.js?v=20260818-10";
-import { iniciarAsistencia } from "./asistencia.js?v=20260818-7";
+import { iniciarEmpleados } from "./empleados.js?v=20260819-1";
+import { iniciarAsistencia } from "./asistencia.js?v=20260819-1";
 import { iniciarAuditoria, iniciarMonitorAuditoriaGlobal } from "./auditoria.js?v=20260818-9";
 import { iniciarDashboard } from "./dashboard.js?v=20260818-7";
 import { iniciarConfiguracion, aplicarAparienciaGuardada, obtenerAparienciaGuardada } from "./configuracion.js?v=20260814-1";
@@ -472,7 +472,7 @@ async function cargarVista(
 
 
         const ruta =
-            `vistas/${vistaPrincipal}.html?v=20260818-9`;
+            `vistas/${vistaPrincipal}.html?v=20260819-1`;
 
 
         const respuesta =
@@ -910,4 +910,3 @@ regla.innerHTML =
 }
 
 }
-
