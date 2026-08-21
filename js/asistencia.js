87df6fde73db511a19dabddee5937bc0bbfa232a
zavalachumpitazoscar <zavalachumpitazoscar@gmail.com>
2026-08-21 17:40:58 -0500

    Update asistencia.js

--- js/asistencia.js
+++ js/asistencia.js
@@ -3,7 +3,7 @@ import { iniciarFechaAsistencia } from "./asistencia/fecha-asistencia.js";
 import { iniciarImportacionMarcaciones } from "./asistencia/importacion-marcaciones.js?v=20260821-3";
 import { iniciarResumenAsistencia } from "./asistencia/resumen-asistencia.js?v=20260820-5";
 import { iniciarMarcacionesAsistencia } from "./asistencia/marcaciones-asistencia.js?v=20260819-1";
-import { iniciarResumenMensualAsistencia } from "./asistencia/resumen-mensual-asistencia.js?v=20260820-8";
+import { iniciarResumenMensualAsistencia } from "./asistencia/resumen-mensual-asistencia.js?v=20260821-9";
 import { iniciarEditarDiaAsistencia } from "./asistencia/editar-dia-asistencia.js?v=20260814-1";
 import { iniciarMarcacionManualAsistencia } from "./asistencia/marcacion-manual-asistencia.js?v=20260817-1";
 import { iniciarAjusteRefrigerioAsistencia } from "./asistencia/ajuste-refrigerio-asistencia.js";
